b9c32861d4403f1d3cb67be1270b9f3a92c73fc6
secretrobotron <secretrobotron@gmail.com>
2012-04-28 18:53:14 -0400

    New engine for CSS parallax n' stuff.

--- src/engine.js
+++ src/engine.js
@@ -1,21 +1,28 @@
 (function(){
 
-  var _parallaxElements;
+  var PARALLAX_FACTOR = 0.1;
 
-  document.addEventListener("mousemove", function(e){
+  var _parallaxElements,
+      _slides;
+
+  window.addEventListener("mousemove", function(e){
     var diffX = e.clientX - window.innerWidth / 2,
         diffY = e.clientY - window.innerHeight / 2;
 
     for (var i = _parallaxElements.length - 1; i >= 0; i--) {
       var element = _parallaxElements[i];
-      element.style.top = (window.innerHeight / 2) - (element.clientHeight / 2) - diffY * 0.05 + "px";
-      element.style.left = (window.innerWidth / 2) - (element.clientWidth / 2) - diffX * 0.05 + "px";
-    };
+      element.style.left = (-diffX - element.clientWidth / 2) * PARALLAX_FACTOR + "px";
+      element.style.top = (-diffY - element.clientHeight / 2) * PARALLAX_FACTOR + "px";
+    }
+  }, false);
 
+  window.addEventListener("click", function(e){
+    console.log("32234234");
   }, false);
 
   document.addEventListener("DOMContentLoaded", function(e){
     _parallaxElements = document.querySelectorAll("*[data-parallax]");
+    _slides = document.getElementsByClassName("slide");
   }, false);
 
 }());
