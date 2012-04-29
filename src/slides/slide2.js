fab797683fc54e4de51c3012b47339feef974aef
secretrobotron <secretrobotron@gmail.com>
2012-04-29 17:32:59 -0400

    Videos and such gone from deck slide.

--- src/slides/slide2.js
+++ src/slides/slide2.js
@@ -1,24 +1,13 @@
 define(["../slide", "../panels"], function(Slide, Panels){
 
-  var slideElement = document.getElementById("ship-slide"),
-      videos = slideElement.querySelectorAll("*[data-choice-panel] video");
-
-  var _panels = new Panels(slideElement);
+  var slideElement = document.getElementById("ship-slide");
 
   var slide = new Slide("ship-slide", {
     update: function(){
     },
     start: function(){
-      _panels.start();
-      for (var i = videos.length - 1; i >= 0; i--) {
-        videos[i].play();
-      };
     },
     stop: function(){
-      _panels.stop();
-      for (var i = videos.length - 1; i >= 0; i--) {
-        videos[i].pause();
-      };
     }
   });
 
