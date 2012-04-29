6c699fc4960ce86749446440bef2c69399f93f12
secretrobotron <secretrobotron@gmail.com>
2012-04-29 18:26:24 -0400

    Different... videos... for chrome...

--- src/slides/slide1.js
+++ src/slides/slide1.js
@@ -12,7 +12,6 @@ define(["../slide", "../rAFLoop", "../tween", "../swipe"], function(Slide, rAFLo
         loaded = 0;
     for (var i = videos.length - 1; i >= 0; i--) {
       if(videos[i].readyState === 4){
-        console.log(videos[i].currentSrc, videos[i].readyState);
         ++loaded;
       }
     }
@@ -39,7 +38,7 @@ define(["../slide", "../rAFLoop", "../tween", "../swipe"], function(Slide, rAFLo
             next();
           }, false);
         }
-      }, 100);
+      }, 500);
     },
     stop: function(){
       _arrowTween.stop();
