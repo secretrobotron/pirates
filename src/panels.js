2a3d760f00db03de55f5694421ba67ddb6aeb145
secretrobotron <secretrobotron@gmail.com>
2012-04-29 12:22:15 -0400

    Movie volume.

--- src/panels.js
+++ src/panels.js
@@ -1,7 +1,8 @@
 define(["tween", "rAFLoop"], function(Tween, rAFLoop){
   
   var MIN_PANEL_WIDTH = 50,
-      GROWTH_FACTOR = 0.7;
+      GROWTH_FACTOR = 0.7,
+      MAX_PANEL_PERCENT = 0.3;
 
   return function(slideElement){
 
@@ -13,23 +14,35 @@ define(["tween", "rAFLoop"], function(Tween, rAFLoop){
     var _leftTween = new Tween(),
         _rightTween = new Tween();
 
+    var _leftMovie = _leftPanel.getElementsByTagName("video")[0],
+        _rightMovie = _rightPanel.getElementsByTagName("video")[0];
+
     var _updateLoop = new rAFLoop(function(){
+      var maxPanelWidth = slideElement.clientWidth * MAX_PANEL_PERCENT,
+          lp = _leftTween.value / maxPanelWidth,
+          rp = _rightTween.value / maxPanelWidth;
       _leftPanel.style.width = Math.max(MIN_PANEL_WIDTH, _leftTween.value) + "px";
       _rightPanel.style.width = Math.max(MIN_PANEL_WIDTH, _rightTween.value) + "px";
+      if(_leftMovie){
+        _leftMovie.volume = lp;
+      }
+      if(_rightMovie){
+        _rightMovie.volume = lp;
+      }
     });
 
     function onMouseMove(e){
       var diff = -(window.innerWidth / 2 - e.clientX),
           slideWidthPortion = slideElement.clientWidth/5,
-          thirdSlideWidth = slideElement.clientWidth/3;
+          maxPanelWidth = slideElement.clientWidth * MAX_PANEL_PERCENT;
       if(diff > slideWidthPortion){
-        _rightTween.set(Math.min(thirdSlideWidth, diff * GROWTH_FACTOR));
+        _rightTween.set(Math.min(maxPanelWidth, diff * GROWTH_FACTOR));
       }
       else{
         _rightTween.set(MIN_PANEL_WIDTH);
       }
       if(diff < -slideWidthPortion){
-        _leftTween.set(Math.min(thirdSlideWidth, -diff * GROWTH_FACTOR));
+        _leftTween.set(Math.min(maxPanelWidth, -diff * GROWTH_FACTOR));
       }
       else{
         _leftTween.set(MIN_PANEL_WIDTH);
