f49968a8ec249442a02bec7a5897efe2addaa89f
Jorrit de Vries <jorrit5477@gmail.com>
2012-04-29 20:12:52 +0200

    Added waitiong slide

--- src/slides/slide3.js
+++ src/slides/slide3.js
@@ -13,39 +13,39 @@ define(["../slide", "../panels"], function(Slide, Panels){
 
   var slide = new Slide("deck-slide", {
 	
-	update: function() {
-		// Move a little with a sin
-		_lastTime += 0.1;
-		_deckBackgroundElement.style.left = Math.sin(_lastTime * 0.2) * 16 + "px";
-		_deckBackgroundElement.style.top = Math.sin(Math.sin(_lastTime)) * 4 + "px";
-		
-		// Check mouse center
-		var centerX = CENTER_RECT[0] + parseInt(_slideElement.style.left);
-		if (((_lastX > centerX) && (_lastX < CENTER_RECT[2] + centerX)) &&
-			((_lastY > CENTER_RECT[1]) && (_lastY < CENTER_RECT[3] + CENTER_RECT[1])))
-		{
-			_ak47Element.style.webkitTransform = _ak47Element.style.transform = "rotate(5deg)";
-			_ak47BlurredElement.style.opacity = 0;
-			_deckBackgroundBlurredElement.style.opacity = 1;
-		}
-		else
-		{
-			_ak47Element.style.webkitTransform = _ak47Element.style.transform = "rotate(-15deg)";
-			_ak47BlurredElement.style.opacity = 1;
-			_deckBackgroundBlurredElement.style.opacity = 0;
-		}
+    update: function() {
+      // Move a little with a sin
+      _lastTime += 0.1;
+      _deckBackgroundElement.style.left = Math.sin(_lastTime * 0.2) * 16 + "px";
+      _deckBackgroundElement.style.top = Math.sin(Math.sin(_lastTime)) * 4 + "px";
+      
+      // Check mouse center
+      var centerX = CENTER_RECT[0] + parseInt(_slideElement.style.left);
+      if (((_lastX > centerX) && (_lastX < CENTER_RECT[2] + centerX)) &&
+        ((_lastY > CENTER_RECT[1]) && (_lastY < CENTER_RECT[3] + CENTER_RECT[1])))
+      {
+        _ak47Element.style.webkitTransform = _ak47Element.style.transform = "rotate(5deg)";
+        _ak47BlurredElement.style.opacity = 0;
+        _deckBackgroundBlurredElement.style.opacity = 1;
+      }
+      else
+      {
+        _ak47Element.style.webkitTransform = _ak47Element.style.transform = "rotate(-15deg)";
+        _ak47BlurredElement.style.opacity = 1;
+        _deckBackgroundBlurredElement.style.opacity = 0;
+      }
     },
-	start: function(){
+    start: function(){
       _panels.start();
-	},
-	stop: function(){
+    },
+    stop: function(){
       _panels.stop();
-	}
+    }
   });
   
   function onMouseMove(e){
     _lastX = e.clientX;  
-	_lastY = e.clientY;
+    _lastY = e.clientY;
   }
 
   _slideElement.addEventListener("mousemove", onMouseMove, false);
